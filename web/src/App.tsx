7c65e530239d8b47b519728a08ebf7cec915699a
Sondre Benjamin Aasen <699902+sobeaa@users.noreply.github.com>
2026-04-09 02:58:03 +0200

    fix(web): restore tagline

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ export function App() {
     <>
       <main>
         <h1>Cloudini</h1>
-        <p>Code in. Infrastructure out.</p>
+        <p>Write code. Summon a serverless app.</p>
       </main>
       <footer>Copyright © 2026 Deasilsoft</footer>
     </>
